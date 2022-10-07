8dfdcf8342e256612a9b14e2d081b87e82fea5cc
jhordan camacho <jhordan.camacho@gmail.com>
2022-10-06 22:20:50 -0500

    [Add] ceros to date

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
+const addCero = (number) => {
+    if (number < 10) {
+        return `0${number}`;
+    }
+    return number;
+};
 setInterval(() => {
     let date = new Date();
-    document.getElementById('date').innerHTML = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    document.getElementById('date').innerHTML = addCero(date.getHours()) + ':' + addCero(date.getMinutes()) + ':' + addCero(date.getSeconds());
 }, 1000);
