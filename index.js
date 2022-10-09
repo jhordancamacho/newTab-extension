ff5d6001fa0b7cf8c6c5cfc85f4ea69fd11da22f
Jhordan Camacho <jhordan.camacho@gmail.com>
2022-10-09 04:04:36 -0500

    [Add] feacture change background img

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// actualizacion de hora
 const addCero = (number) => {
     if (number < 10) {
         return `0${number}`;
@@ -9,3 +9,27 @@ setInterval(() => {
     let date = new Date();
     document.getElementById('date').innerHTML = addCero(date.getHours()) + ':' + addCero(date.getMinutes()) + ':' + addCero(date.getSeconds());
 }, 1000);
+
+// saber si a cargado una imagen
+if (localStorage.getItem("imagen")) {
+    document.getElementById("imagen-fondo").src = localStorage.getItem("imagen");
+}
+
+// handle events
+function selectImage() {
+    document.querySelector(".container-setting>input").click();
+}
+document.querySelector(".container-setting").addEventListener("click", selectImage);
+
+function cambiarImagen(e) {
+    input = e.target;
+    console.log(input.files[0])
+    let srcImagenNueva = input.files[0];
+    let reader = new FileReader();
+    reader.onload = function () {
+        localStorage.setItem("imagen", this.result);
+        document.getElementById("imagen-fondo").src = this.result;
+    }
+    reader.readAsDataURL(srcImagenNueva);
+}
+document.getElementById("upload-image").addEventListener("input", (e) => cambiarImagen(e))
